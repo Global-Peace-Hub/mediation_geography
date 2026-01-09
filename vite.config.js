e6cbed97b5df5d4a7c531c91ae41ed95cafd5428
Tomas Vancisin <tvancisi@ed.ac.uk>
2026-01-09 16:23:18 +0000

    fiirst

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/elisa_damico/'
+  base: '/mediation_geography/'
 })
